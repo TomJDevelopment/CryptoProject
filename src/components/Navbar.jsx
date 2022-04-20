ccfa244d7ecd5b3b7fe65174b5d02e5cfb973a38
Tom Johnson <42745512+TomJDevelopment@users.noreply.github.com>
2022-04-20 11:53:43 +0100

    Working on animations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
+import React from "react";
 import { HiMenuAlt4 } from 'react-icons/hi'
 import { AiOutlineClose } from 'react-icons/ai'
+import { useSpring, animated } from 'react-spring'
 
 import logo from '../../images/logo.png'
 
@@ -12,6 +14,9 @@ const NavbarItem = ({ title, classProps }) => {
 }
 
 const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = React.useState(false);
+    const animationProps = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } });
+
     return (
         <nav className="w-full flex md:justify-center justify-between items-center p-4">
             <div className="md:flex-[0.5] justify-center items-center">
@@ -25,6 +30,23 @@ const Navbar = () => {
                     Login
                 </li>
             </ul>
+            <div className="flex relative">
+                {toggleMenu
+                ? <AiOutlineClose fontSize={28}  className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(false)} />
+                : <HiMenuAlt4 fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)} /> }
+                {toggleMenu && (
+                    /* This will only show when toggleMenu is true, also the - in front of "right" means minus */
+                    /* TODO Sort out animations using React Spring or an alternative method */
+                    <animated.ul style={animationProps} className="z-index fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white">
+                        <li className="text-xl w-full my-2">
+                            <AiOutlineClose onClick={() => setToggleMenu(false)} />
+                        </li>
+                        {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+                            <NavbarItem key={item + index} title={item} classProps="my-2 text-lg" />
+                        ))}
+                    </animated.ul>
+                )}
+            </div>
         </nav>
     )
 }
