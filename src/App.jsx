c57fa830eb26892831e6d73b00df69f673e426af
Tom Johnson <42745512+TomJDevelopment@users.noreply.github.com>
2022-05-30 11:37:07 +0100

    Transactions sorted and displaying properly

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,12 @@ const App = () => {
         <Welcome />
       </div>
         <div className="w-full gradient-bg-transactions p-4">
-            <h1 className="text-3xl sm:text-5xl pb-3 text-white text-gradient py-1 md:subpixel-antialiased text-center">Latest Transactions</h1>
-            <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-8 gap-y-4 m-6 overflow-y-hidden overflow-x-auto">
-                {connectedAccount ? previousTransactions ? previousTransactions.reverse().map((transaction, i) => (
+            <h1 className="text-3xl sm:text-5xl pb-3 text-white text-gradient py-1 md:subpixel-antialiased text-center">Latest 25 Transactions</h1>
+            <div className="flex flex-nowrap overflow-y-hidden overflow-x-visible scrollbar">
+                {connectedAccount ? previousTransactions ? previousTransactions.slice(0, 25).reverse().map((transaction, i) => (
                     <Transaction AddressFrom={transaction.AddressFrom} AddressTo={transaction.AddressTo} Keyword={transaction.Keyword} Message={transaction.Message} Amount={transaction.Amount} key={i} />
-                )) : <p className="text-xl sm:text-2xl text-white justify-self-center">No Transactions Available</p>
-                    : <p className="text-xl sm:text-2xl text-white justify-self-center">Please connect your MetaMask to see previous transactions</p>}
+                )) : <p className="text-xl sm:text-2xl text-white col-span-full justify-self-center">No Transactions Available</p>
+                    : <p className="text-xl sm:text-2xl text-white col-span-full justify-self-center">Please connect your MetaMask to see previous transactions</p>}
             </div>
         </div>
       <Footer />
--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -20,14 +20,15 @@ const Transaction = ({ AddressFrom, AddressTo, Keyword, Message, Amount }) => {
     });
 
     return (
-        <div className="bg-gray-700 rounded-2xl">
-            <img className="rounded-tl-2xl rounded-tr-2xl w-full h-4/5" alt="GIF" src={url} />
+        <div className="bg-gray-700 flex-none h-full w-96 m-6 rounded-2xl hover:scale-105 duration-300">
+            <img className="rounded-tl-2xl rounded-tr-2xl w-full h-80" alt="GIF" src={url} />
             <div className="grid grid-cols-2">
-                <p className="pl-3 pt-2 text-white font-semibold text-left">From: {shortenAddress(AddressFrom)}</p>
-                <p className="pr-3 pt-2 text-white font-semibold text-right">To: {shortenAddress(AddressTo)}</p>
+                <p className="pl-3 pt-2 text-white font-semibold text-left">From {shortenAddress(AddressFrom)}</p>
+                <p className="pr-3 pt-2 text-white font-semibold text-right">To {shortenAddress(AddressTo)}</p>
             </div>
-            <p className="pl-3 pb-2 text-white font-semibold text-left">Message: {Message}</p>
-            <button type="button" className="rounded-tl-2xl rounded-tr-2xl text-white font-sans font-semibold w-full border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer hover:bg-blue-600 transition-colors duration-300">View</button>
+            <p className="pl-3 text-white font-semibold text-left">Sent &mdash; {Amount} Ethereum</p>
+            <p className="pl-3 pb-2 text-white font-semibold text-left">Saying &mdash; {Message}</p>
+            <button type="button" className="rounded-bl-2xl rounded-br-2xl justify-self-end text-white font-sans font-semibold w-full border-[1px] p-2 border-[#3d4f7c] cursor-pointer hover:bg-blue-600 transition-colors duration-300">View</button>
         </div>
     )
 }
